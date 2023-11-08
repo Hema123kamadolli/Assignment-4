489f11663de0a38330c0f248e24ab32b3d8fb136
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-11-08 16:03:16 +0530

    Assignment-4

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMaking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            
+            if (number > 0) //Check if the number is positive, negative, zero
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+
+            
+            string evenOrOdd = (number % 2 == 0) ? "even" : "odd"; // Check if the number is even or odd using the conditional operator
+            Console.WriteLine("The number is " + evenOrOdd + ".");
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
